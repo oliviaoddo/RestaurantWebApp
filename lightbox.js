e20dac769d36a145342188fe1c33a01600e55c35
oliviaoddo <olivia.oddo@gmail.com>
2016-11-15 20:22:49 -0800

    lightbox added - when a product it clicked it is overlayed on the page

---
+++ lightbox.js
@@ -0,0 +1,46 @@
+//Problem: User when clicking on image goes to a dead end
+//Solution: Create an overlay with the large image - Lightbox
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+//An image to overlay
+$overlay.append($image);
+
+//A caption to overlay
+$overlay.append($caption);
+
+//Add overlay
+$("body").append($overlay);
+
+//Capture the click event on a link to an image
+$("#productImage a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  //Update overlay with the image linked in the link
+  $image.attr("src", imageLocation);
+  
+  //Show the overlay.
+  $overlay.show();
+  
+  //Get child's alt attribute and set caption
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+//When overlay is clicked
+$overlay.click(function(){
+  //Hide the overlay
+  $overlay.hide();
+});
+
+
+
+
+
+
+
+
+
+
