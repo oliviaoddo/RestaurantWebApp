eaaeec0e5cf5c5f6c0511eb88599206064aba5ef
oliviaoddo <olivia.oddo@gmail.com>
2016-11-15 18:26:16 -0800

    created a separate file for the customer log in form, php code is separated

--- customer_login.php
+++ customer_login.php
@@ -3,7 +3,7 @@ session_start();
 //log customer out by unsetting "customer" in SESSION based on whether "customer" is set in _SESSION or not
 if (isset($_SESSION["customer"])) {
 	unset($_SESSION["customer"]);
-	header("location: customer_login.php");
+	header("location: login.php");
 }
 ?>
 <?php
@@ -30,81 +30,9 @@ if(isset($_POST["username"]) && isset($_POST["password"])){
 	}
   
 	else{ 
-		echo 'That information is incorrect, try again <a href="customer_login.php">Click Here</a>';
+		echo 'That information is incorrect, try again <a href="login.php">Click Here</a>';
 		exit();
 	}
   
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Log in</title>
-
-    <!-- Bootstrap core CSS -->
-   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-    <link href="mangiabene.css" rel="stylesheet">
-    
-  </head>
-
-  <body>
-  
- <div>
-    <?php include_once("nav.php");?>
-</div>
-<div align="center" id="mainWrapper">
-  <div id="pageContent"><br />
-    <div align="left" style="margin-left:24px;">
-      <h2>Customer Login</h2>
-      <form id="form1" name="form1" method="post" action="customer_login.php">
-        User Name:<br />
-          <input name="username" type="text" id="username" size="40" />
-        <br /><br />
-        Password:<br />
-       <input name="password" type="password" id="password" size="40" />
-       <br />
-	   <br />
-	    <a href="customer_signup.php">Don't have an account? Sign up here!</a> 
-       <br />
-       <br />
-       <br />
-       
-         <input type="submit" name="button" id="button" value="Log In" />
-       
-      </form>
-      <p>&nbsp; </p>
-    </div>
-    <br />
-  <br />
-  <br />
-  </div>
-</div>
- <div>
-    <?php include_once("footer.php");?>
-</div>
-</body>
-</html>
