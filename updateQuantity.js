88276fce2255ad670fe898994b9e01e8a1b42eb6
oliviaoddo <olivia.oddo@gmail.com>
2016-12-01 15:42:55 -0800

    quantity select works in cart, user can choose new quantity and the product quantity in local storage updates

--- updateQuantity.js
+++ updateQuantity.js
@@ -1,18 +1,11 @@
-function updateQuantity(idToUpdate){
-	var cartEntries = JSON.parse(localStorage.getItem("allEntries"));
-	//$(document).on('mousedown', '#select'+idToUpdate, function(event){
-	//	console.log("yes");
-	//}
-	$('#select'+idToUpdate).mousedown(function(){
-		console.log("hello");
+function updateQuantity(idToUpdate, i){
+	$('#select'+idToUpdate).change(function(){
+		var cartEntries = JSON.parse(localStorage.getItem("allEntries"));
+		var selectedQuantity = $('#select'+idToUpdate).find(":selected").text();
+		cartEntries[i].productQuantity = selectedQuantity;
+		cartEntries[i].productTotal = parseInt(cartEntries[i].productPrice) * parseInt(selectedQuantity);
+		localStorage.setItem("allEntries", JSON.stringify(cartEntries));
+		window.location.reload(); 
 	})
-	//console.log($('#select'+idToUpdate));
-	//var selectId = $('#select'+cartEntries[idToUpdate].productId);
-
-	//var valueOfQuantity = $('#row'+cartEntries[idToUpdate].productId).find(selectId).find("select option:selected").val;
-	//console.log(valueOfQuantity);
-	//get the selected id
-	//set cartEntries[idToUpate].productQuantity
-
 
 }
