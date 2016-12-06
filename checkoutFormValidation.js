b79a722ab0cbf9b04338188f334c1f6599a5fa2d
oliviaoddo <olivia.oddo@gmail.com>
2016-12-06 10:40:02 -0800

    added to checkout form validation, times and delivery address still needs to be implemented

--- checkoutFormValidation.js
+++ checkoutFormValidation.js
@@ -13,7 +13,7 @@ errorFName.before(fname);
 fNameInput.addEventListener('invalid', function(event){
     event.preventDefault();
     if ( ! event.target.validity.valid ) {
-        fname.textContent   = 'First name should only contain letters';
+        fname.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: First name should only contain letters';
         fname.className     = 'error';
         fname.style.display = 'block';
     }
@@ -38,7 +38,7 @@ errorLName.before(lname);
 lNameInput.addEventListener('invalid', function(event){
     event.preventDefault();
     if ( ! event.target.validity.valid ) {
-        lname.textContent   = 'Last name should only contain letters';
+        lname.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Last name should only contain letters';
         lname.className     = 'error';
         lname.style.display = 'block';
     }
@@ -63,14 +63,14 @@ errorEmail.before(email);
 emailInput.addEventListener('invalid', function(event){
     event.preventDefault();
     if ( ! event.target.validity.valid ) {
-        email.textContent   = 'Invalid email ex. example@lotsoffood.com';
+        email.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Invalid email ex. example@lotsoffood.com';
         email.className     = 'error';
         email.style.display = 'block';
     }
 });
 
 emailInput.addEventListener('input', function(event){
-    if ( 'block' === lname.style.display ) {
+    if ( 'block' === email.style.display ) {
         emailInput.className = '';
         email.style.display = 'none';
     }
@@ -88,14 +88,14 @@ errorPhone.before(phone);
 phoneInput.addEventListener('invalid', function(event){
     event.preventDefault();
     if ( ! event.target.validity.valid ) {
-        phone.textContent   = 'Invalid phone, follow the pattern "xxx-xxx-xxxx"';
+        phone.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Invalid phone, follow the pattern "xxx-xxx-xxxx"';
         phone.className     = 'error';
         phone.style.display = 'block';
     }
 });
 
 phoneInput.addEventListener('input', function(event){
-    if ( 'block' === lname.style.display ) {
+    if ( 'block' === phone.style.display ) {
         phoneInput.className = '';
         phone.style.display = 'none';
     }
@@ -113,14 +113,14 @@ errorRadio.before(radio);
 radioInput.addEventListener('invalid', function(event){
     event.preventDefault();
     if ( ! event.target.validity.valid ) {
-        radio.textContent   = 'Pickup or delivery must be selected';
+        radio.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Pickup or delivery must be selected';
         radio.className     = 'error';
         radio.style.display = 'block';
     }
 });
 
 radioInput.addEventListener('input', function(event){
-    if ( 'block' === lname.style.display ) {
+    if ( 'block' === radio.style.display ) {
         radioInput.className = '';
         radio.style.display = 'none';
     }
@@ -138,20 +138,21 @@ errorCard.before(card);
 cardInput.addEventListener('invalid', function(event){
     event.preventDefault();
     if ( ! event.target.validity.valid ) {
-        card.textContent   = 'Invalid card number, must be between 13 and 16 numbers';
+        card.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Invalid card number, must be between 13 and 16 numbers';
         card.className     = 'error';
         card.style.display = 'block';
     }
 });
 
 cardInput.addEventListener('input', function(event){
-    if ( 'block' === lname.style.display ) {
+    if ( 'block' === card.style.display ) {
         cardInput.className = '';
         card.style.display = 'none';
     }
 });
 
 /*expiration date*/
+/*
 var expirationMonth = document.getElementByClass('card_month');
 var monthError  = document.getElementById('expirationError');
 
@@ -174,5 +175,204 @@ expirationMonth.addEventListener('input', function(event){
         expirationMonth.className = '';
         month.style.display = 'none';
     }
+});*/
+
+/*card code*/
+var codeInput = document.getElementById('cardCode');
+var errorCode  = document.getElementById('errorCode');
+
+var code = document.createElement('div');
+code.id = 'notify';
+code.style.display = 'none';
+errorCode.before(code);
+
+codeInput.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        code.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Invalid CSC, must be 3 numbers';
+        code.className     = 'error';
+        code.style.display = 'block';
+    }
 });
 
+codeInput.addEventListener('input', function(event){
+    if ( 'block' === code.style.display ) {
+        codeInput.className = '';
+        code.style.display = 'none';
+    }
+});
+
+/*billing fname*/
+var billingFName = document.getElementById('billingFName');
+var errorBillFName  = document.getElementById('errorBillFName');
+
+var billF = document.createElement('div');
+billF.id = 'notify';
+billF.style.display = 'none';
+errorBillFName.before(billF);
+
+billingFName.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        billF.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: First name should only contain letters';
+        billF.className     = 'error';
+        billF.style.display = 'block';
+    }
+});
+
+billingFName.addEventListener('input', function(event){
+    if ( 'block' === billF.style.display ) {
+        billingFName.className = '';
+        billF.style.display = 'none';
+    }
+});
+
+/*billing lname*/
+var billingLName = document.getElementById('billingLName');
+var errorBillLName  = document.getElementById('errorBillLName');
+
+var billL = document.createElement('div');
+billL.id = 'notify';
+billL.style.display = 'none';
+errorBillLName.before(billL);
+
+billingLName.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        billL.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Last name should only contain letters';
+        billL.className     = 'error';
+        billL.style.display = 'block';
+    }
+});
+
+billingLName.addEventListener('input', function(event){
+    if ( 'block' === billL.style.display ) {
+        billingLName.className = '';
+        billL.style.display = 'none';
+    }
+});
+
+/*billing city*/
+var billingCity = document.getElementById('billingCity');
+var errorBillCity  = document.getElementById('errorBillCity');
+
+var billCity = document.createElement('div');
+billCity.id = 'notify';
+billCity.style.display = 'none';
+errorBillCity.before(billCity);
+
+billingCity.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        billCity.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: City must only contain letters';
+        billCity.className     = 'error';
+        billCity.style.display = 'block';
+    }
+});
+
+billingCity.addEventListener('input', function(event){
+    if ( 'block' === billCity.style.display ) {
+        billingCity.className = '';
+        billCity.style.display = 'none';
+    }
+});
+
+/*billing state*/
+var billingState = document.getElementById('billingState');
+var errorBillState  = document.getElementById('errorBillState');
+
+var billState = document.createElement('div');
+billState.id = 'notify';
+billState.style.display = 'none';
+errorBillState.before(billState);
+
+billingState.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        billState.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: State must only contain letters';
+        billState.className     = 'error';
+        billState.style.display = 'block';
+    }
+});
+
+billingState.addEventListener('input', function(event){
+    if ( 'block' === billState.style.display ) {
+        billingState.className = '';
+        billState.style.display = 'none';
+    }
+});
+
+/*billing zip*/
+var billingZip = document.getElementById('billingZip');
+var errorBillZip  = document.getElementById('errorBillZip');
+
+var billZip = document.createElement('div');
+billZip.id = 'notify';
+billZip.style.display = 'none';
+errorBillZip.before(billZip);
+
+billingZip.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        billZip.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Invalid zip code, ex. 92677 or 92677-0008';
+        billZip.className     = 'error';
+        billZip.style.display = 'block';
+    }
+});
+
+billingZip.addEventListener('input', function(event){
+    if ( 'block' === billZip.style.display ) {
+        billingZip.className = '';
+        billZip.style.display = 'none';
+    }
+});
+
+/*billing country*/
+var billingCountry = document.getElementById('billingCountry');
+var errorBillCountry  = document.getElementById('errorBillCountry');
+
+var billCoun = document.createElement('div');
+billCoun.id = 'notify';
+billCoun.style.display = 'none';
+errorBillCountry.before(billCoun);
+
+billingCountry.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        billCoun.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required: Country must only contain letters';
+        billCoun.className     = 'error';
+        billCoun.style.display = 'block';
+    }
+});
+
+billingCountry.addEventListener('input', function(event){
+    if ( 'block' === billCoun.style.display ) {
+        billingCountry.className = '';
+        billCoun.style.display = 'none';
+    }
+});
+
+/*billing country*/
+var billingStreet = document.getElementById('billingStreet');
+var errorBillStreet  = document.getElementById('errorBillStreet');
+
+var billStreet = document.createElement('div');
+billStreet.id = 'notify';
+billStreet.style.display = 'none';
+errorBillStreet.before(billStreet);
+
+billingStreet.addEventListener('invalid', function(event){
+    event.preventDefault();
+    if ( ! event.target.validity.valid ) {
+        billStreet.innerHTML   = '<i style = "color: red;" class="fa fa-exclamation-circle" aria-hidden="true"></i> Required';
+        billStreet.className     = 'error';
+        billStreet.style.display = 'block';
+    }
+});
+
+billingStreet.addEventListener('input', function(event){
+    if ( 'block' === billStreet.style.display ) {
+        billingCountry.className = '';
+        billStreet.style.display = 'none';
+    }
+});
